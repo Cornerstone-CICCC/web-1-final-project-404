55b631dee54ca9e24e0bd16c8735429d5d82d3c6
Christian Jimenez <cajt@outlook.com>
2024-11-14 11:28:55 -0800

    Need little adjustments in images

--- js/porfolio.js
+++ js/porfolio.js
@@ -1,7 +1,27 @@
 const buttons = document.querySelectorAll("h5");
 
+// sections //
+catalogD = document.querySelector(".desktop_catalog");
+catalogP = document.querySelector(".phone");
+// phone
 const makeup = document.querySelector(".makeup");
 const hair = document.querySelector(".hair");
+// desktop
+const makeupD = document.querySelector(".makeup_D");
+const hairD = document.querySelector(".hair_D");
+
+
+// add evento for resize
+window.addEventListener("resize", () => {
+    if(window.innerWidth > 1200){
+        catalogD.style.display = "grid";
+        catalogP.style.display = "none";
+    }
+    else{
+        catalogD.style.display = "none";
+        catalogP.style.display = "grid";
+    }
+})
 
 // add event listeners to the buttons
 buttons.forEach(button => { 
@@ -10,10 +30,19 @@ buttons.forEach(button => {
             button.classList.remove("active");
             buttons[1].classList.add("active");
             // images //
-            if(makeup.classList.contains("hide")){
-                makeup.classList.add("show")
-                makeup.classList.remove("hide")
-                hair.classList.add("hide");
+            if(window.innerWidth > 1200){
+                if(makeupD.classList.contains("hide")){
+                    makeupD.classList.add("show");
+                    makeupD.classList.remove("hide");
+                    hairD.classList.add("hide");
+                }
+            }
+            else{
+                if(makeup.classList.contains("hide")){
+                    makeup.classList.add("show");
+                    makeup.classList.remove("hide");
+                    hair.classList.add("hide");
+                }
             }
             
         }
@@ -21,10 +50,19 @@ buttons.forEach(button => {
             button.classList.remove("active");
             buttons[0].classList.add("active");
             // images //
-            if(hair.classList.contains("hide")){
-                hair.classList.remove("hide");
-                makeup.classList.add("hide")
-                makeup.classList.remove("show")
+            if(window.innerWidth > 1200){
+                if(hairD.classList.contains("hide")){
+                    hairD.classList.remove("hide");
+                    makeupD.classList.add("hide");
+                    makeupD.classList.remove("show");
+                }
+            }
+            else{
+                if(hair.classList.contains("hide")){
+                    hair.classList.remove("hide");
+                    makeup.classList.add("hide");
+                    makeup.classList.remove("show")
+                }
             }
         }
     })
