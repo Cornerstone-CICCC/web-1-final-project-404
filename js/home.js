a7bad699204ae8b224242aa4b5db0cacee5eed9b
mizukifujikawa-ciccc <mz.fujikawa.ciccc@gmail.com>
2024-11-14 08:43:49 -0800

    home middle

---
+++ js/home.js
@@ -0,0 +1,16 @@
+
+(function() {
+    emailjs.init('2UvQwli4N-kG1yMxe');
+})();
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        emailjs.sendForm('service_ciccc', 'template_0gibnm4', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
